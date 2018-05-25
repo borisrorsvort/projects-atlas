8785ff245b02fceeb1854a68294d5b7a95c3494c
Boris Rorsvort <boris.rorsvort@gmail.com>
2018-05-25 17:32:41 +0300

    Extract world object

--- src/main.js
+++ src/main.js
@@ -2,102 +2,32 @@ import "./stylesheets/main.css";
 
 import * as THREE from "three";
 
-// Created by Bjorn Sandvik - thematicmapping.org
+import World from "./world";
+
 (function() {
   var webglEl = document.getElementById("webgl");
+  var world = new World();
 
-  // if (!Detector.webgl) {
-  //   Detector.addGetWebGLMessage(webglEl);
-  //   return;
-  // }
+  const { camera, scene, sphere, clouds, renderer } = world;
 
-  let width = window.innerWidth;
-  let height = window.innerHeight;
-
-  // Earth params
-  let radius = 0.5;
-  let segments = 32;
-  let rotation = 10.5;
-
-  let scene = new THREE.Scene();
-
-  let camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
-  camera.position.z = 1.5; // How far from us
-
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize(width, height);
-
-  scene.add(new THREE.AmbientLight(0xcccccc));
-
-  var light = new THREE.DirectionalLight(0xffffff, 0.2);
-  light.position.set(5, 3, 5);
-  scene.add(light);
-
-  var sphere = createSphere(radius, segments);
-  sphere.rotation.y = rotation;
-  scene.add(sphere);
-
-  var clouds = createClouds(radius, segments);
-  clouds.rotation.y = rotation;
-  scene.add(clouds);
-
-  var stars = createStars(90, 64);
-  scene.add(stars);
-
-  // var controls = new THREE.TrackballControls(camera);
+  world.init();
 
   webglEl.appendChild(renderer.domElement);
 
   render();
 
   function render() {
-    // controls.update();
     sphere.rotation.y += 0.0005;
     clouds.rotation.y += 0.0005;
     requestAnimationFrame(render);
     renderer.render(scene, camera);
   }
-
-  function createSphere(radius, segments) {
-    return new THREE.Mesh(
-      new THREE.SphereGeometry(radius, segments, segments),
-      new THREE.MeshPhongMaterial({
-        map: new THREE.TextureLoader().load("./images/2_no_clouds_4k.jpg"),
-        bumpMap: new THREE.TextureLoader().load("./images/elev_bump_4k.jpg"),
-        bumpScale: 0.005,
-        specularMap: new THREE.TextureLoader().load("./images/water_4k.png"),
-        specular: new THREE.Color("grey"),
-      }),
-    );
-  }
-
-  function createClouds(radius, segments) {
-    return new THREE.Mesh(
-      new THREE.SphereGeometry(radius + 0.003, segments, segments),
-      new THREE.MeshPhongMaterial({
-        map: new THREE.TextureLoader().load("./images/fair_clouds_4k.png"),
-        transparent: true,
-      }),
-    );
-  }
-
-  function createStars(radius, segments) {
-    return new THREE.Mesh(
-      new THREE.SphereGeometry(radius, segments, segments),
-      new THREE.MeshBasicMaterial({
-        map: new THREE.TextureLoader().load("./images/galaxy_starfield.png"),
-        side: THREE.BackSide,
-      }),
-    );
-  }
 })();
 
-if (ENV !== "production") {
-  // Enable LiveReload
-  document.write(
-    '<script src="http://' +
-      (location.host || "localhost").split(":")[0] +
-      ':35729/livereload.js?snipver=1"></' +
-      "script>",
-  );
-}
+// Enable LiveReload
+document.write(
+  '<script src="http://' +
+    (location.host || "localhost").split(":")[0] +
+    ':35729/livereload.js?snipver=1"></' +
+    "script>",
+);
---
+++ src/world.js
@@ -0,0 +1,107 @@
+import * as THREE from "three";
+
+export default class World {
+  constructor() {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.radius = 0.5;
+    this.segments = 32;
+    this.rotation = 10.5;
+    this.scene = new THREE.Scene();
+    this.sphere = this.createSphere();
+    this.clouds = this.createClouds();
+    this.stars = this.createStars();
+    this.camera = this.createCamera();
+    this.renderer = new THREE.WebGLRenderer();
+  }
+
+  createCamera() {
+    return new THREE.PerspectiveCamera(
+      45,
+      this.width / this.height,
+      0.01,
+      1000,
+    );
+  }
+
+  createSphere() {
+    return new THREE.Mesh(
+      new THREE.SphereGeometry(this.radius, this.segments, this.segments),
+      new THREE.MeshPhongMaterial({
+        map: new THREE.TextureLoader().load("./images/2_no_clouds_4k.jpg"),
+        bumpMap: new THREE.TextureLoader().load("./images/elev_bump_4k.jpg"),
+        bumpScale: 0.005,
+        specularMap: new THREE.TextureLoader().load("./images/water_4k.png"),
+        specular: new THREE.Color("grey"),
+      }),
+    );
+  }
+
+  createClouds() {
+    return new THREE.Mesh(
+      new THREE.SphereGeometry(
+        this.radius + 0.003,
+        this.segments,
+        this.segments,
+      ),
+      new THREE.MeshPhongMaterial({
+        map: new THREE.TextureLoader().load("./images/fair_clouds_4k.png"),
+        transparent: true,
+      }),
+    );
+  }
+
+  createStars(radius = 90, segments = 64) {
+    return new THREE.Mesh(
+      new THREE.SphereGeometry(radius, segments, segments),
+      new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load("./images/galaxy_starfield.png"),
+        side: THREE.BackSide,
+      }),
+    );
+  }
+
+  initRenderer() {
+    console.log(this.width, this.height);
+
+    this.renderer.setSize(this.width, this.height);
+  }
+
+  initCamera() {
+    this.camera.position.z = 1.5;
+  }
+
+  initSphere() {
+    this.sphere.rotation.y = this.rotation;
+    this.scene.add(this.sphere);
+  }
+
+  initClouds() {
+    this.clouds.rotation.y = this.rotation;
+    this.scene.add(this.clouds);
+  }
+
+  initStars() {
+    this.scene.add(this.stars);
+  }
+
+  initLightings() {
+    const light = new THREE.DirectionalLight(0xffffff, 0.2);
+    light.position.set(5, 3, 5);
+    this.scene.add(new THREE.AmbientLight(0xcccccc));
+    this.scene.add(light);
+  }
+
+  init() {
+    this.initCamera();
+    this.initRenderer();
+    this.initSphere();
+    this.initClouds();
+    this.initStars();
+    this.initLightings();
+  }
+
+  render() {
+    this.renderer.render(this.scene, this.camera);
+  }
+}
